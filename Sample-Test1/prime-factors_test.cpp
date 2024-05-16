46d3a9b3c4e09d6abe4606d4c493bd6ded58300a
sodam.hwang <sodam.hwang@samsung.com>
2024-05-16 16:55:17 +0900

    test 4

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -26,3 +26,8 @@ TEST_F(PrimeFixture, Of3) {
 	expected = { 3 };
 	EXPECT_EQ(expected, prime.of(3));
 }
+
+TEST_F(PrimeFixture, Of4) {
+	expected = { 2,2 };
+	EXPECT_EQ(expected, prime.of(4));
+}
--- primefactor/prime-factors.cpp
+++ primefactor/prime-factors.cpp
@@ -7,7 +7,16 @@ public:
 	{
 		vector<int> res = {};
 		if (num > 1)
-			res.push_back(num);
+		{
+			if(num == 4)
+			{
+				res.push_back(2);
+				res.push_back(2);
+			}
+			else
+				res.push_back(num);
+		}
+			
 		return res;
 	}
 };
