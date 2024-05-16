e086262e514dfa061d99136233390fbed5e4a43f
sodam.hwang <sodam.hwang@samsung.com>
2024-05-16 16:53:40 +0900

    primefactor refac

--- primefactor/prime-factors.cpp
+++ primefactor/prime-factors.cpp
@@ -3,13 +3,11 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int i)
+	vector<int> of(int num)
 	{
 		vector<int> res = {};
-		if (i == 2)
-			res.push_back(2);
-		if (i == 3)
-			res.push_back(3);
+		if (num > 1)
+			res.push_back(num);
 		return res;
 	}
 };
